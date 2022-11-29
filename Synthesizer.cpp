8c0f4973a3b41a1eccefd03b0ef206a34a410441
Tóth Gergő <tgery3@gmail.com>
2022-11-29 16:17:48 +0100

    moving constants to another file

--- Synthesizer.cpp
+++ Synthesizer.cpp
@@ -7,35 +7,7 @@ using namespace std;
 #include "Output.h"
 #include "MidiHandler.h"
 #include "Filter.h"
-
-const short SINE_WAVE = 1;
-const short TRIANGLE_WAVE = 2;
-const short SQUARE_WAVE = 3;
-const short WHITE_NOISE = 4;
-const short SAWTOOTH_WAVE = 5;
-
-const short PLAYMODE_KEYBOARD = 1;
-const short PLAYMODE_MIDI = 2;
-
-const int DEFAULT_SAMPLE_RATE = 44100;
-const short DEFAULT_OUTPUT_MODE = 1;
-const short DEFAULT_BLOCK_COUNT = 8;
-const int DEFAULT_BLOCK_SAMPLE = 512;
-
-const double ADSR_MAX_VALUE = 0.1;
-const double ADSR_ATTACK_TIME = 0.2;
-const double ADSR_DECAY_TIME = 0.1;
-const double ADSR_SUSTAIN_LEVEL = 0.1;
-const double ADSR_RELEASE_TIME = 0.1;
-
-const double BASE_KEYBOARD_FREQUENCY = 261.63;
-const double BASE_MIDI_FREQUENCY = 8.1758;
-
-const int LPF_TAPS = 50;
-const double LPF_CUTOFF_FREQUENCY = 150.0;
-
-const int WHITE_NOISE_MIN_FREQ = 10;
-const int WHITE_NOISE_MAX_FREQ = 20000;
+#include "Constants.cpp"
 
 bool filterEnabled = false;
 bool isEnvelope = false;
